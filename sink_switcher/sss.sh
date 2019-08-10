eb04caf0b1430ae6116dd8fbe6a6e9c7e85f050e
Arteev Aleksey <alarteev@yandex.ru>
2019-08-10 23:40:03 +0500

    sound sink switcher: output description

--- sink_switcher/sss.sh
+++ sink_switcher/sss.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 # Sound sink switcher
-# Version 1.0 / August 10,2019
+# Version 1.1 / August 10,2019
 
 current=`pacmd stat | grep "Default sink name" | cut -d':' -f 2 | sed -e 's/^[ ]*//'`
-sinks=`pacmd list-sinks | grep "name:" | cut -d':' -f 2`
+sinks=`pacmd list-sinks | grep "name:" | cut -d':' -f 2 | sed -e 's/^[ ]*//'`
+desc=`pacmd list-sinks | grep "device.description" |  cut -d'=' -f 2 | sed -e 's/^[ ]*//'` 
+
+IFS=$'\n'
+descriptions=()
+for s in ${desc} 
+do
+  descriptions+=($s)
+done
+
+ 
 arr=()
 current_index=-1
 index=0
-printf "current: %s\n" $current
 for item in ${sinks} 
 do
     name=${item:1:-1}
@@ -18,13 +27,14 @@ do
     index=$(($index+1))
 done
 
-arr_len=${#arr[@]}
+#printf "current: %s\n" ${descriptions[$current_index]}
+
 next=$(($current_index+1))
-if [ $next -ge $arr_len ]; then 
+if [ $next -ge ${#arr[@]} ]; then 
     next=0
 fi
 
 next_name=${arr[$next]}
-printf "switch to: %s\n" $next_name
+echo ${descriptions[$next]}
 pacmd set-default-sink $next_name        
 pacmd move-sink-input 0 $next_name
