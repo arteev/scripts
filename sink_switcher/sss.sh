6145ad99e27de2561b16408acc641c2c656a3bc7
Arteev Aleksey <alarteev@yandex.ru>
2019-08-11 21:39:17 +0500

    correct switching of sink inputs

--- sink_switcher/sss.sh
+++ sink_switcher/sss.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Sound sink switcher
-# Version 1.1 / August 10,2019
+# Version 1.2 / August 11,2019
 
 current=`pacmd stat | grep "Default sink name" | cut -d':' -f 2 | sed -e 's/^[ ]*//'`
 sinks=`pacmd list-sinks | grep "name:" | cut -d':' -f 2 | sed -e 's/^[ ]*//'`
 desc=`pacmd list-sinks | grep "device.description" |  cut -d'=' -f 2 | sed -e 's/^[ ]*//'` 
+inputraw=`pacmd list-sink-inputs | grep "index:" |  cut -d':' -f 2 | sed -e 's/^[ ]*//'`
 
 IFS=$'\n'
 descriptions=()
@@ -13,6 +14,11 @@ do
   descriptions+=($s)
 done
 
+inputs=()
+for s in ${inputraw} 
+do
+  inputs+=($s)
+done
  
 arr=()
 current_index=-1
@@ -36,5 +42,8 @@ fi
 
 next_name=${arr[$next]}
 echo ${descriptions[$next]}
-pacmd set-default-sink $next_name        
-pacmd move-sink-input 0 $next_name
+pacmd set-default-sink $next_name   
+for inp in ${inputs}
+do
+    pacmd move-sink-input $inp $next_name      
+done
